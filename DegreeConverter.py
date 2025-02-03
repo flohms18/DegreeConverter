f2dbee63ca24add4f0ae9c25f3a38573a6a1f95e
flotr18 <trouilletflorent@outlook.com>
2025-02-03 19:10:11 +0100

    Fixed Issue when printing

--- DegreeConverter.py
+++ DegreeConverter.py
@@ -1,11 +1,20 @@
-def hello():
-    Unituser = input("Enter the unit you want to convert from:(F/C) ")
-    if Unituser == "C":
+def Converter():
+    UnitUser = input("Enter the unit you want to convert from:(F/C) ")
+    NumberUser = input("Enter the number you want to convert: ")
+
+    if UnitUser == "C":
         print("You have selected Celsius")
-    elif Unituser == "F":
+        NumberUser = int(NumberUser)
+        ConvertedNumber = NumberUser * 9/5 + 32
+        print(str(ConvertedNumber) + " °C")
+
+    elif UnitUser == "F":
         print("You have selected Fahrenheit")
+        ConvertedNumber = (int(NumberUser) - 32) * 5/9
+        print(str(ConvertedNumber) + " °F")
+
     else:
         print("Invalid unit")
-        hello()
+        print("Please enter C or F")
 
-hello()
+Converter()
